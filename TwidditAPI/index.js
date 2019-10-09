602e6ec17a7206b3101f2cb66c1db19e2ea5250c
Luca Nixdorf <luca@familie-nixdorf.de>
2019-10-09 22:50:15 +0200

    Added the jwt checkign middleware for oidc

--- TwidditAPI/index.js
+++ TwidditAPI/index.js
@@ -2,9 +2,28 @@ var http = require('http');
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
-var sample = require('./routes/routes');
+var api = require('./routes/routes');
 var app = express();
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://www.googleapis.com/oauth2/v3/certs`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: '341498580870-6ft6jad0dj6g1rtni435dg9kvn1r5phb.apps.googleusercontent.com',
+  issuer: `https://accounts.google.com`,
+  algorithms: ['RS256']
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
@@ -12,7 +31,7 @@ app.use(bodyParser.urlencoded({'extended':'false'}));
 //Put your angular dist folder here
 app.use(express.static(path.join(__dirname, '../TwidditClient/dist/Twiddit')));
 app.use('/', express.static(path.join(__dirname, '../TwidditClient/dist/Twiddit')));
-app.use('/api', sample);
+app.use('/api', checkJwt, api);
 
 var port = parseInt(process.env.PORT || '4300');
 app.set('port', port);
